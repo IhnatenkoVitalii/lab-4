935a12ffe0adc5b6f104958b4a9a30d312c64664
Ihnatenko Vitalii <Ihnatenko Vitalii@DESKTOP-DGTQ2P4>
2023-01-12 11:23:54 +0200

    Добавьте файлы проекта.

---
+++ lab-4/lab-4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int n = 15;
+    int x[n] = { 11,12,13,14,15 };
+    int y[n] = {};
+
+    for (int i = 0; i < n; i++)
+    {
+        y[i] = i;
+        cout << y[i] << ",";
+    }
+}
